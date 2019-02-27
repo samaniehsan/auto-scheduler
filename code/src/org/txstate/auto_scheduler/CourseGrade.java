748bf2eb5d5a904b2e7914020ded561232e83b2d
Ehsan <Ehsan@samani.com>
2019-02-26 18:18:55 -0600

    student grade service

---
+++ code/src/org/txstate/auto_scheduler/CourseGrade.java
@@ -0,0 +1,11 @@
+package org.txstate.auto_scheduler;
+
+public enum CourseGrade {
+	Incomplete,
+	F,
+	E,
+	D,
+	C,
+	B,
+	A
+}
---
+++ code/src/org/txstate/auto_scheduler/StudentRecordRepositoryImpl.java
@@ -0,0 +1,10 @@
+package org.txstate.auto_scheduler;
+
+import java.util.Collection;
+
+public class StudentRecordRepositoryImpl implements StudentRecordRepository {
+
+	public Collection<StudentRecord> get(String studentId) {
+		throw new UnsupportedOperationException("Implement by reading from csv");
+	}
+}
