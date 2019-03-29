abab7707de58a295f6c41122c8498345a178914e
brominger <47297228+brominger@users.noreply.github.com>
2019-03-29 12:16:47 -0500

    Updated test with new info

--- tests/CourseInfoTest.java
+++ tests/CourseInfoTest.java
@@ -9,15 +9,15 @@ public class CourseInfoTest {
     @Test
     public void testSetGetCourseNumber() {
         CourseInfo course = new CourseInfo();
-        course.setCourse_number("XXX1234");
-        assertTrue(course.getCourse_number()== "XXX1234");
+        course.setCourseNumber("XXX1234");
+        assertTrue(course.getCourseNumber()== "XXX1234");
     }
 
     @Test
     public void testSetGetCourseName() {
         CourseInfo course = new CourseInfo();
-        course.setCourse_name("Course Name");
-        assertTrue(course.getCourse_name()== "Course Name");
+        course.setCourseName("Course Name");
+        assertTrue(course.getCourseName()== "Course Name");
     }
 
     @Test
@@ -28,15 +28,52 @@ public class CourseInfoTest {
     }
 
     @Test
+    public void testSetGettimeDay() {
+        CourseInfo course = new CourseInfo();
+        course.setDayTime("0:00 - 0:00 D");
+        assertTrue(course.getDayTime()== "0:00 - 0:00 D");
+    }
+
+    @Test
+    public void testSetGetRoomNumber() {
+        CourseInfo course = new CourseInfo();
+        course.setRoomNumber(1234);
+        assertTrue(course.getRoomNumber()== 1234);
+    }
+
+    @Test
+    public void testSetGetSectionNumber() {
+        CourseInfo course = new CourseInfo();
+        course.setSectionNumber(0);
+        assertTrue(course.getRoomNumber()== 0);
+    }
+
+    @Test
+    public void testIsFull() {
+        CourseInfo course = new CourseInfo();
+        course.setFull(true);
+        assertTrue(course.isFull());
+        course.setFull(false);
+        assertFalse(course.isFull());
+    }
+
+
+    @Test
     public void testToString()
     {
         CourseInfo course = new CourseInfo();
-        course.setCourse_number("XXX1234");
-        course.setCourse_name("Course Name");
+        course.setCourseNumber("XXX1234");
+        course.setCourseName("Course Name");
         course.setSubject("Subject");
-        String expected = "Course Number = XXX1234, Course Name = Course Name, Subject = Subject\n";
+        course.setDayTime("0:00 - 0:00 D");
+        course.setRoomNumber(1234);
+        String expected = "Course Number: XXX1234" +
+                "\nSection Number: 0" +
+                "\nCourse Name: Course Name" +
+                "\nSubject: Subject" +
+                "\nTime/Day: 0:00 - 0:00 D" +
+                "\nRoom Number: 1234\n";
         assertEquals(expected, course.toString());
     }
-
 
 }
