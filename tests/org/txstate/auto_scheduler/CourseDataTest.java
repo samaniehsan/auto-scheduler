8155806bcf87d943a8ed97053c60b10e20ecfad9
brominger <47297228+brominger@users.noreply.github.com>
2019-04-01 11:01:56 -0500

    Added tests
    added tests for createSection, addRoomTimeDay and courseRandomnizer

--- tests/org/txstate/auto_scheduler/CourseDataTest.java
+++ tests/org/txstate/auto_scheduler/CourseDataTest.java
@@ -2,24 +2,14 @@ package org.txstate.auto_scheduler;
 
 import org.apache.commons.csv.CSVFormat;
 import org.junit.Test;
-
-
-
 import org.hamcrest.collection.IsEmptyCollection;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.hamcrest.CoreMatchers.*;
-
-
 import static org.junit.Assert.*;
 
 public class CourseDataTest {
 
-    public CourseDataTest() {
-        this.loadData();
-    }
 
     @Test
     public void loadData() {
@@ -36,5 +26,37 @@ public class CourseDataTest {
         assertTrue(courses.get(0).getCourseNumber().equals("NUMBER"));
         assertTrue(courses.get(0).getCourseName().equals("Name"));
         assertTrue(courses.get(0).getSubject().equals("Subject"));
+    }
+	
+	@Test
+    public void createSection() {
+        List<CourseInfo> courses = new ArrayList<CourseInfo>();
+        CourseInfo courseTemp = new CourseInfo();
+        courseTemp.setCourseNumber("XXX123");
+        courses.add(courseTemp);
+        CourseData.createSection(courses);
+        assertThat(courses.size(), is(4));
+        courses.clear();
+        courseTemp.setCourseNumber("XXX323");
+        courses.add(courseTemp);
+        CourseData.createSection(courses);
+        assertThat(courses.size(), is(3));
+    }
+	
+	    @Test
+    public void addRoomTimeDay() {
+        List<CourseInfo> courses = new ArrayList<CourseInfo>();
+        CourseInfo courseTemp = new CourseInfo();
+        courses.add(courseTemp);
+        CourseData.addRoomTimeDay(courses);
+        assertTrue(courses.get(0).getRoomNumber() == 1000);
+        assertTrue(courses.get(0).getDayTime().equals("8:00 - 9:20 MW"));
+    }
+
+    @Test
+    public void courseRandomnizer() {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list = CourseData.courseRandomnizer(2);
+        assertThat(list.size(), is(2));
     }
 }
