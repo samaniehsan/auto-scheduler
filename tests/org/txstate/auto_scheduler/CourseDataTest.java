c6102cff97c3bdebea0091c6543994d170129916
brominger <47297228+brominger@users.noreply.github.com>
2019-04-01 09:46:17 -0500

    Update CourseDataTest.java

--- tests/org/txstate/auto_scheduler/CourseDataTest.java
+++ tests/org/txstate/auto_scheduler/CourseDataTest.java
@@ -17,10 +17,6 @@ import static org.junit.Assert.*;
 
 public class CourseDataTest {
 
-    public CourseDataTest() {
-        this.loadData();
-    }
-
     @Test
     public void loadData() {
         CourseData courses = new CourseData();
