6d4c1e08e6692131473ac074886b20b7c0c457ff
Ehsan <Ehsan@samani.com>
2019-04-13 15:42:36 -0500

    show how to retrieve all files in resources folder
    this code will simply print all the files in the resources folder to the
    cnosole standard output.

--- src/main/java/auto_batcher/Main.java
+++ src/main/java/auto_batcher/Main.java
@@ -1,10 +1,28 @@
 package org.txstate.auto_batcher;
 
+import java.io.*;
 
 public class Main 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World Auto Batcher!" );
+        printFolders();
     }    
+
+    public static void printFolders() {
+        File folder = new File("./resources/data/records");
+        if( folder != null) {
+            File[] listOfFiles = folder.listFiles();
+            if(listOfFiles != null ) {
+                for (int i = 0; i < listOfFiles.length; i++) {
+                    if (listOfFiles[i].isFile() && listOfFiles[i] != null) {
+                      System.out.println("File " + listOfFiles[i].getName());
+                    } else if (listOfFiles[i].isDirectory()) {
+                      System.out.println("Directory " + listOfFiles[i].getName());
+                    }
+                  }
+            }
+        }        
+    }
 }
