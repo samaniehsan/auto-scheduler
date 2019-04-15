8afbfd2850f72cd294804e2fba0aa05c7466782a
Val <vjara8@gmail.com>
2019-04-15 10:50:22 -0500

    initial json parsing

--- src/main/java/auto_batcher/Main.java
+++ src/main/java/auto_batcher/Main.java
@@ -1,10 +1,69 @@
 package org.txstate.auto_batcher;
 
+import java.util.*;
+import java.io.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-public class Main 
+public class Main
 {
-    public static void main( String[] args )
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World Auto Batcher!" );
-    }    
+    	JSONParser jsonParser = new JSONParser();
+    	JSONArray allStudentsList;
+    	File folder = new File("/auto-scheduler/src/main/resources/data/records");
+    	File[] listOfFiles = folder.listFiles();
+
+
+    	for (int i = 0; i < listOfFiles.length; i++) {
+
+        	if (listOfFiles[i].isFile()) {
+	            String fileName = listOfFiles[i].getName();
+	            // String basename = FilenameUtils.getBaseName(fileName);
+        	}
+
+	        try (FileReader reader = new FileReader(listOfFiles[i]))
+	        {
+	            Object obj = jsonParser.parse(reader);
+	 
+	            JSONArray studentList = (JSONArray) obj;
+	            // System.out.println(studentList);
+	             
+	            for(JSONObject student : studentList){
+
+	            	parseStudentObject(student, fileName);
+	            	
+	            	//add to allStudentsList
+
+	            }
+	 
+	        } catch (FileNotFoundException e) {
+	            e.printStackTrace();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        } catch (ParseException e) {
+	            e.printStackTrace();
+	        }
+
+
+        }
+
+    }
+ 
+    private static void parseStudentObject(JSONObject student, String fileName)
+    {
+        JSONObject studentObject = (JSONObject) student.get(fileName);
+         //test to see if it 
+        String year = (String) studentObject.get("year");   
+        System.out.println(year);
+         
+    }
 }
