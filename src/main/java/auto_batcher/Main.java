003e788a68202956f2192cddb89bc7c4c1ea133c
vjara98 <vjara8@gmail.com>
2019-04-16 10:21:01 -0500

    Initial json parsing (#40)
    * initial json parsing
    
    * modified fileName variable
    
    * Ehsan editing Val's code

--- src/main/java/auto_batcher/Main.java
+++ src/main/java/auto_batcher/Main.java
@@ -1,10 +1,78 @@
 package org.txstate.auto_batcher;
 
+import java.util.*;
+import java.io.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-public class Main 
+public class Main
 {
-    public static void main( String[] args )
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World Auto Batcher!" );
-    }    
+		try {
+			runParser();
+		} catch(Exception e) {
+			e.printStackTrace(System.err);
+		}
+	}
+	
+	private static void runParser() throws ParseException {
+		JSONParser jsonParser = new JSONParser();
+    	JSONArray allStudentsList;
+    	String fileName;
+    	File folder = new File("resources/data/records");
+		if(folder == null ) {
+			return;
+		}
+
+		File[] listOfFiles = folder.listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.toLowerCase().endsWith(".json");
+			}
+		});
+
+		if(listOfFiles == null) {
+			return;
+		}
+
+    	for (int i = 0; i < listOfFiles.length; i++) {
+
+        	if (listOfFiles[i].isFile()) {
+				fileName = listOfFiles[i].getName();
+				
+				try (FileReader reader = new FileReader(listOfFiles[i]))
+				{
+					Object obj = jsonParser.parse(reader);
+		 
+					JSONObject studentBio = (JSONObject) obj;
+					parseStudentObject(studentBio);
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+        	}
+
+        }
+	}
+ 
+    private static void parseStudentObject(JSONObject student)
+    {
+        Long year = (Long) student.get("year");   
+        System.out.println(year);
+         
+    }
 }
+
