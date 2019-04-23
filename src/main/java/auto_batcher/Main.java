d944d9f7f6976178a1c5695b84f5971ebf3776b1
samaniehsan <ehsan.samani-social@outlook.com>
2019-04-23 17:04:06 -0500

    Val's PR: parsing and randomly grouping students. missing grouping by grade level (#48)
    * parsing and randomly grouping students. missing grouping by grade level
    
    * Adding guard statments so unit tests dont fail
    
    change formatting for better bracket indendation
    Also adding in guard statements to overcome null point exception.

--- src/main/java/auto_batcher/Main.java
+++ src/main/java/auto_batcher/Main.java
@@ -14,65 +14,25 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class Main
-{
-    @SuppressWarnings("unchecked")
-    public static void main(String[] args)
-    {
+public class Main {
+	public static void main(String[] args) {
 		try {
-			runParser();
-		} catch(Exception e) {
+
+			String newFile = "resources/data/records";
+			ArrayList<Student> groupedStudents = new ArrayList<Student>();
+
+			Invoker invoker = new Invoker(newFile);
+			File[] files = invoker.runParser();
+			if (files !=null && files.length != 0) {
+				StudentInfoRepo studentInfoRepo = new StudentInfoRepo(files);
+				studentInfoRepo.obtainJSON();
+				StudentGrouper studenGrouper = new StudentGrouper(studentInfoRepo.getRepo());
+				studenGrouper.randomGrouping();
+				groupedStudents = studenGrouper.getStudentList();
+			}
+
+		} catch (Exception e) {
 			e.printStackTrace(System.err);
 		}
 	}
-	
-	private static void runParser() throws ParseException {
-		JSONParser jsonParser = new JSONParser();
-    	JSONArray allStudentsList;
-    	String fileName;
-    	File folder = new File("resources/data/records");
-		if(folder == null ) {
-			return;
-		}
-
-		File[] listOfFiles = folder.listFiles(new FilenameFilter() {
-			public boolean accept(File dir, String name) {
-				return name.toLowerCase().endsWith(".json");
-			}
-		});
-
-		if(listOfFiles == null) {
-			return;
-		}
-
-    	for (int i = 0; i < listOfFiles.length; i++) {
-
-        	if (listOfFiles[i].isFile()) {
-				fileName = listOfFiles[i].getName();
-				
-				try (FileReader reader = new FileReader(listOfFiles[i]))
-				{
-					Object obj = jsonParser.parse(reader);
-		 
-					JSONObject studentBio = (JSONObject) obj;
-					parseStudentObject(studentBio);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				} catch (ParseException e) {
-					e.printStackTrace();
-				}
-        	}
-
-        }
-	}
- 
-    private static void parseStudentObject(JSONObject student)
-    {
-        Long year = (Long) student.get("year");   
-        System.out.println(year);
-         
-    }
 }
-
---
+++ src/main/java/auto_batcher/Student.java
@@ -0,0 +1,28 @@
+package org.txstate.auto_batcher;
+
+import java.util.*;
+import java.io.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Student
+{
+	private FirstLastName name;
+	private Long year;
+
+	public Student(FirstLastName name, Long year){
+		this.name = name;
+		this.year = year;
+	}
+	
+}
+
