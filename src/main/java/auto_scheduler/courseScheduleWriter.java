a25b27b367fc2a75932d16716c554d81141a3850
brominger <47297228+brominger@users.noreply.github.com>
2019-04-25 10:27:44 -0500

    Added time slot to the course schedule creation

--- src/main/java/auto_scheduler/courseScheduleWriter.java
+++ src/main/java/auto_scheduler/courseScheduleWriter.java
@@ -1,40 +1,76 @@
 package org.txstate.auto_scheduler;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 
 
 public class courseScheduleWriter {
 
-    private static final String COURSES = "./data/courseSchedule.csv";
+    //private static final String COURSES = "./resources/courseSchedule.csv";
+
+    //Delimiter used in CSV file
+    private static final String NEW_LINE_SEPARATOR = "\n";
+
+    //CSV file header
+    private static final Object [] FILE_HEADER = {"Course_Number", "Section_Number", "Course_Name","Time_Slot","Time_Day","Room_Number","Capacity","Enrolled"};
 
     public static void main(String[] args) throws IOException {
 
-        CourseData getCourses = new CourseData();
-        List<CourseInfo> courses = getCourses.loadData();
+        FileWriter fileWriter = null;
 
-        try (
-                BufferedWriter writer = Files.newBufferedWriter(Paths.get(COURSES));
+        CSVPrinter csvPrinter = null;
 
-                CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT
-                        .withHeader("Course_Number", "Section_Number", "Course_Name", "Time_Day","Room_Number","Capacity","Enrolled"));
-        ) {
+        CSVFormat csvFileFormat = CSVFormat.DEFAULT.withRecordSeparator(NEW_LINE_SEPARATOR);
+
+        try {
+
+            //initialize FileWriter object
+            fileWriter = new FileWriter("resources/data/courseSchedule/courseSchedule.csv") ;
+
+            //initialize CSVPrinter object
+            csvPrinter = new CSVPrinter(fileWriter, csvFileFormat);
+
+            //Create CSV file header
+            csvPrinter.printRecord(FILE_HEADER);
+            CourseData getCourses = new CourseData();
+            List<CourseInfo> courses = getCourses.loadData();
             for (int i = 0; i < (courses.size()); i++)
                 csvPrinter.printRecord(courses.get(i).getCourseNumber(),
-                        courses.get(i).getCourseNumber(),
                         courses.get(i).getSectionNumber(),
                         courses.get(i).getCourseName(),
+                        courses.get(i).getTimeSlot(),
                         courses.get(i).getDayTime(),
                         courses.get(i).getRoomNumber(),
                         courses.get(i).getCapacity(),
                         courses.get(i).getEnrolled());
+
             csvPrinter.flush();
+
+
+            System.out.println("CSV file was created successfully !!!");
+
+        } catch (Exception e) {
+            System.out.println("Error in CsvFileWriter !!!");
+            e.printStackTrace();
+        } finally {
+            try {
+                fileWriter.flush();
+                fileWriter.close();
+                csvPrinter.close();
+            } catch (IOException e) {
+                System.out.println("Error while flushing/closing fileWriter/csvPrinter !!!");
+                e.printStackTrace();
+            }
         }
+
+
+
     }
 }
 
