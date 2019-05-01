a2297cccb4f0d811fcdf1ec471fcf309bbe7b6d3
brominger <47297228+brominger@users.noreply.github.com>
2019-05-01 14:25:12 -0500

    Override to string and change what gets return.

--- src/main/java/auto_scheduler/ScheduleCancelation.java
+++ src/main/java/auto_scheduler/ScheduleCancelation.java
@@ -2,8 +2,9 @@ package org.txstate.auto_scheduler;
 
 import java.io.IOException;
 
-public class ScheduleCancelation implements ScheduleAction {
+public class ScheduleCancelation extends ScheduleSet implements ScheduleAction {
 	ResourcePathProvider pathProvider;
+	String filename;
 
 	public ScheduleCancelation(ResourcePathProvider pathProvider) {
 		this.pathProvider = pathProvider;
@@ -12,7 +13,13 @@ public class ScheduleCancelation implements ScheduleAction {
 		DefaultResourcePathProviderImpl path = new DefaultResourcePathProviderImpl();
 		String studentId = context.getStudentId();
 		StudentScheduleRepositoryImpl schedule = new StudentScheduleRepositoryImpl(path);
+		filename = studentId + "_schedule.cvs";
 		schedule.delete(studentId);
-		return null;
+		return this;
+	}
+
+	@Override
+	public String toString() {
+		return "The following file were deleted " + filename +".";
 	}
 }
