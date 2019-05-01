9695cb2b8e3b17050c931aa54d8631ff2090fbd7
samaniehsan <ehsan.samani-social@outlook.com>
2019-05-01 18:09:01 -0500

    remove unused code (#76)

--- src/main/java/auto_scheduler/StudentCourseGradeService.java
+++
@@ -1,8 +0,0 @@
-package org.txstate.auto_scheduler;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-public interface StudentCourseGradeService {
-	boolean hasCourse(String studentId,String courseCode) throws FileNotFoundException, IOException;
-}
