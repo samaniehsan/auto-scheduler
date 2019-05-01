3c58cbd92c9741beb9f3ce91d1f150578a1f93ca
Barry Rominger <brominge@gmail.com>
2019-05-01 10:59:09 -0500

    implemeted ScheduleCancelation.java

--- src/main/java/auto_scheduler/ScheduleCancelation.java
+++ src/main/java/auto_scheduler/ScheduleCancelation.java
@@ -1,13 +1,18 @@
 package org.txstate.auto_scheduler;
 
+import java.io.IOException;
+
 public class ScheduleCancelation implements ScheduleAction {
 	ResourcePathProvider pathProvider;
 
 	public ScheduleCancelation(ResourcePathProvider pathProvider) {
 		this.pathProvider = pathProvider;
 	}
-	public ScheduleSet execute(OperationContext context)
-	{
+	public ScheduleSet execute(OperationContext context) throws IOException {
+		DefaultResourcePathProviderImpl path = new DefaultResourcePathProviderImpl();
+		String studentId = context.getStudentId();
+		StudentScheduleRepositoryImpl schedule = new StudentScheduleRepositoryImpl(path);
+		schedule.delete(studentId);
 		return null;
 	}
 }
