67f94c3e578fdf472adc96b0d749f61051dbba6d
samaniehsan <ehsan.samani-social@outlook.com>
2019-05-01 23:15:45 -0500

    Master fix auto batcher by putting code in right folder (#81)
    * auto_batcher's jar file file invoker to right folder
    
    * basic fix for invoking auto scheduler

--- run.cmd
+++ run.cmd
@@ -1,4 +1,2 @@
-#!/usr/bin/env bash
-java -jar ./auto_scheduler/target/auto_scheduler-1.0.jar
-
-java -jar ./auto_batcher/target/auto_batcher-1.0.jar
+cd auto_batcher/target/
+java -jar auto_batcher-1.0.jar "C:\github_dot_com\auto-scheduler\auto_scheduler\target\auto_scheduler\target\auto_scheduler-1.0.jar"
--- src/main/java/auto_batcher/StudentScheduleInvoker.java
+++ src/main/java/auto_batcher/StudentScheduleInvoker.java
@@ -1,13 +1,53 @@
 package org.txstate.auto_batcher;
-import java.lang.UnsupportedOperationException; 
-
+import java.lang.UnsupportedOperationException;
+import java.io.IOException;
+/**
+ * StudentScheduleInvoker.java : Will compaile the Student ID into a java command to run auto_scheduler
+ * @param N/A
+ * @return N/A
+ * @exception N/A
+ * @version 1.0
+ * @since   5-2-2019
+ * */
 public class StudentScheduleInvoker {
     String autoSchedulerJarPath;
     StudentScheduleInvoker(String autoSchedulerJarPath) {
         this.autoSchedulerJarPath = autoSchedulerJarPath;
     }
-    
-    public void schedule(String studentId) {
-        throw new UnsupportedOperationException("Derek please implment");
+
+    /**
+     * MultiStudentScheduleInvoker : Will enter a custom command for the respective id
+     * @param Collection<Student> students
+     * @return N/A
+     * @exception IOException
+     */
+    public void schedule(String studentId) throws java.lang.InterruptedException {
+        // Example command:
+        // java auto_scheduler-1.0.jar --schedule AXYZID
+        String javaExecutablePrefix = "java -jar ";
+        String pathexe = javaExecutablePrefix + pathMaker(studentId);
+        System.out.println("Made it");
+        System.out.println(this.autoSchedulerJarPath);
+        try {
+            Runtime rt = Runtime.getRuntime();
+            Process proc = rt.exec(pathexe);
+            int exitVal = proc.waitFor();
+            System.out.println("scheduler exitValue: " + exitVal);
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+        return;
+    }
+    /**
+     * pathMaker : Will create a path to be exectured
+     * @param String id
+     * @return String Path
+     * @exception N/A
+     */
+    public String pathMaker(String id) {
+        String path = "";
+        path = this.autoSchedulerJarPath + " --schedule " + id;
+        return path;
     }
 }
