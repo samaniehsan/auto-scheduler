6b2405cebd05d9ce7a023baa3d876bc76a06b2c1
samaniehsan <ehsan.samani-social@outlook.com>
2019-05-02 16:29:48 -0500

    Course Prioritization && CourseInfoTests (#86)
    * test prioritization order of courses
    
    * Adding Curriculum Course Tests

--- src/main/java/auto_scheduler/CurriculumCourse.java
+++ src/main/java/auto_scheduler/CurriculumCourse.java
@@ -1,5 +1,7 @@
 package org.txstate.auto_scheduler;
 
+import java.lang.IllegalArgumentException;
+
 public class CurriculumCourse extends CourseBase {
     private String preRequisite;
 
@@ -8,6 +10,25 @@ public class CurriculumCourse extends CourseBase {
     }
 
     public void setPreRequisite(String preRequisite) {
+        if(
+            preRequisite != null && 
+            preRequisite.equalsIgnoreCase(this.getCourseNumber())) {
+            throw new IllegalArgumentException(
+                "circular reference;" + preRequisite + 
+                "cannot be be required by courseNumber:"+ this.getCourseNumber());
+        }
         this.preRequisite = preRequisite;
+    }
+
+    @Override
+    public void setCourseNumber(String courseNumber) {
+        if(
+            courseNumber != null && 
+            courseNumber.equalsIgnoreCase(this.getPreRequisite())) {
+            throw new IllegalArgumentException(
+                "circular reference;" + courseNumber + 
+                "cannot be be required by dependent on :"+ this.getCourseNumber());
+        }        
+        this.courseNumber = courseNumber;
     }
 }
