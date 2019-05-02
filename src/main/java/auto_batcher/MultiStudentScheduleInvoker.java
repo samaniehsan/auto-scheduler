6083796ec1dcab2c9f2ffa00672fed8840e4ea01
Ehsan <Ehsan@samani.com>
2019-05-01 22:46:21 -0500

    auto_batcher's jar file file invoker to right folder

--- src/main/java/auto_batcher/MultiStudentScheduleInvoker.java
+++ src/main/java/auto_batcher/MultiStudentScheduleInvoker.java
@@ -1,17 +1,47 @@
 package org.txstate.auto_batcher;
 
 import java.util.Collection;
-import java.lang.UnsupportedOperationException; 
+import java.lang.UnsupportedOperationException;
 
-
+/**
+ * MultiStudentScheduleInvoker.java : Will Traverse Collection<Student> students and pass the respective year to StudentScheduleInvoker
+ * @param N/A
+ * @return N/A
+ * @exception N/A
+ * @version 1.0
+ * @since   5-2-2019
+ */
 public class MultiStudentScheduleInvoker {
     
     StudentScheduleInvoker singleScheduler;
+    /**
+     * MultiStudentScheduleInvoker : Will create a StudentScheduleInvoker variable for file
+     * @param Collection<Student> students
+     * @return N/A
+     * @exception N/A
+     */
     public MultiStudentScheduleInvoker(StudentScheduleInvoker singleScheduler) {
         this.singleScheduler =  singleScheduler; 
     }
 
+    /**
+     * scheduleAlla : Will Call StudentScheduleInvoker for each ID
+     * @param Collection<Student> students
+     * @return N/A
+     * @exception N/A
+     */
     public void scheduleAll(Collection<Student> students) {
-        throw new UnsupportedOperationException("Derek please loop header");
+        //Student -> NamTuble -> getID()
+        for (Student studentExample : students) {
+            //Sr
+            if (studentExample.getYear() == 4){ singleScheduler.schedule(studentExample.getStudentId()); }
+            //Jr
+            else if (studentExample.getYear() == 3) { singleScheduler.schedule(studentExample.getStudentId()); }
+            // Soph
+            else if (studentExample.getYear() == 2) { singleScheduler.schedule(studentExample.getStudentId()); }
+            // Fr
+            else if (studentExample.getYear() == 1) { singleScheduler.schedule(studentExample.getStudentId()); }
+        }
+        return;
     }
 }
--- src/main/java/auto_batcher/StudentScheduleInvoker.java
+++ src/main/java/auto_batcher/StudentScheduleInvoker.java
@@ -1,13 +1,52 @@
 package org.txstate.auto_batcher;
-import java.lang.UnsupportedOperationException; 
-
+import java.lang.UnsupportedOperationException;
+import java.io.IOException;
+/**
+ * StudentScheduleInvoker.java : Will compaile the Student ID into a java command to run auto_scheduler
+ * @param N/A
+ * @return N/A
+ * @exception N/A
+ * @version 1.0
+ * @since   5-2-2019
+ * */
 public class StudentScheduleInvoker {
     String autoSchedulerJarPath;
     StudentScheduleInvoker(String autoSchedulerJarPath) {
         this.autoSchedulerJarPath = autoSchedulerJarPath;
     }
-    
+
+    /**
+     * MultiStudentScheduleInvoker : Will enter a custom command for the respective id
+     * @param Collection<Student> students
+     * @return N/A
+     * @exception IOException
+     */
     public void schedule(String studentId) {
-        throw new UnsupportedOperationException("Derek please implment");
+        // Example command:
+        // java auto_scheduler-1.0.jar --schedule AXYZID
+        String pathexe = "";
+        pathexe = pathMaker(studentId);
+        System.out.println("Made it");
+        System.out.println(this.autoSchedulerJarPath);
+        try {
+            Runtime rt = Runtime.getRuntime();
+            Process pr = rt.exec(pathexe);
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+        return;
+    }
+    /**
+     * pathMaker : Will create a path to be exectured
+     * @param String id
+     * @return String Path
+     * @exception N/A
+     */
+    public String pathMaker(String id) {
+        String path = "";
+        path = this.autoSchedulerJarPath + " --schedule " + id + " \n";
+        System.out.println(path);
+        return path;
     }
 }
